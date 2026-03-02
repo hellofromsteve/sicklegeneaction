d6b27c7c2f3a100aa5283a4efcd8d1fab394a8aa
Stephen <asarestephen.asare@gmail.com>
2026-03-02 16:57:14 +0000

    blog removed

--- resources/views/pages/general/home.blade.php
+++ resources/views/pages/general/home.blade.php
@@ -144,7 +144,7 @@
         <div class="container">
             <div class="section-title text-center">
                 <h1 class="text-danger">What We Do</h1>
-                <p class="text-black">Explore stories, insights, and updates on the fight against Sickle Cell Disease.</p>
+{{--                <p class="text-black">Explore stories, insights, and updates on the fight against Sickle Cell Disease.</p>--}}
             </div>
             <div class="row text-center">
                 <div class="row text-center">
@@ -172,77 +172,6 @@
         </div>
     </div>
     <!-- What We Do End -->
-
-    <!-- Blog Area Start -->
-    <div class="blog-area pt-70 pb-30 pt-sm-60 pb-sm-30">
-        <div class="container">
-            <div class="section-title text-center">
-                <h1 class="text-danger">Our Blog</h1>
-                <p class="text-black">Explore stories, insights, and updates on the fight against Sickle Cell Disease.</p>
-            </div>
-            <div class="row">
-                <!-- Single Blog Start -->
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="ht-single-blog">
-                        <div class="ht-blog-img">
-                            <a href="#"><img src="{{ asset('img/blog/gbcblog.jpg') }}" alt=""></a>
-                        </div>
-                        <div class="ht-blog-text">
-                            <h2><a href="#">Decentralize Hydroxyurea for widespread accessibility.</a></h2>
-                            <div class="post-meta-wapper">
-                                <span class="s-post-meta"><i class="zmdi zmdi-calendar-check"></i>Nov 27, 2023</span>
-                                <span class="s-post-meta"><i class="zmdi zmdi-assignment-account"></i> By: <a href="#">GBC Ghana Online</a></span>
-                            </div>
-                            <p>Sickle cell experts have emphasised the importance of early detection of sickle ...</p>
-                            <a href="#" class="default-btn mt-10">Read More</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Single Blog End -->
-                <!-- Single Blog Start -->
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="ht-single-blog">
-                        <div class="ht-blog-img">
-                            <a href="javascript:void(0)"><img src="{{ asset('img/blog/launchnew.png') }}" alt="Launch Logo"></a>
-                        </div>
-                        <div class="ht-blog-text">
-                            <h2><a href="javascript:void(0)">Foundation launched, Urges Call To Action.</a></h2>
-                            <div class="post-meta-wapper">
-                                <span class="s-post-meta"><i class="zmdi zmdi-calendar-check"></i>Nov 30, 2023</span>
-                                <span class="s-post-meta"><i class="zmdi zmdi-assignment-account"></i>By: <a href="#">Citi News Room</a></span>
-                            </div>
-                            <p>Sickle Gene Action Foundation (SGAF) has been launched in Accra with a call on...</p>
-                            <a href="javascript:void(0)" class="default-btn mt-10">Read More</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Single Blog End -->
-                <!-- Single Blog Start -->
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="ht-single-blog">
-                        <div class="ht-blog-img">
-                            <a href="javascript:void(0)"><img src="{{ asset('img/blog/gnablog.jpg') }}" alt="Uniting Against SCD"></a>
-                        </div>
-                        <div class="ht-blog-text">
-                            <h2><a href="javascript:void(0)">Uniting Against Sickle Cell Disease in Ghana</a></h2>
-                            <div class="post-meta-wapper">
-                                <span class="s-post-meta"><i class="zmdi zmdi-calendar-check"></i>May 23, 2022</span>
-                                <span class="s-post-meta"><i class="zmdi zmdi-assignment-account"></i>By: <a href="#">GNA </a></span>
-                            </div>
-                            <p>Sickle Gene Action Foundation (SGAF), a Sickle Cell Disease (SCD) advocacy...</p>
-                            <a href="javascript:void(0)" class="default-btn mt-10">Read More</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Single Blog End -->
-            </div>
-
-            <div class="text-center">
-            <div class="hlp-btn mt-20"><a href="{{ route('about-us.inspiration') }}" class="default-btn">See More Articles</a></div>
-            </div>
-        </div>
-    </div>
-    <!-- Blog Area End -->
 
 
     <!-- Donate Text Area Start -->
